662e4eb4fd47eece23eea5bbc81b1b3b7ef592d5
Misheel31 <misheelrai7@gmail.com>
2026-09-25 21:17:37 +0545

    Add admin pages and portfolio search

--- gen-media/src/App.jsx
+++ gen-media/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Academy from "./pages/Academy";
 import AcademyDetail from "./pages/AcademyDetail";
 import AdminCourses from "./pages/Admin/AdminCourses";
